b401d45776c24e3593b5f200064fa652c9b00aa5
Mark Greenwood <mrkjgreenwood@gmail.com>
2019-06-13 15:36:52 -0700

    Fully functioning node project

--- create.sh
+++ create.sh
@@ -1,8 +1,15 @@
-function initialize_and_install {
+#!/usr/bin/env bash
+
+DIRECTORY=$1
+
+function initialize_project {
   git init
   npx license $(npm get init-license) -o "$(npm get init-author-name)" > LICENSE
   npx gitignore node
   npm init -y
+}
+
+function install_dev_dependencies {
   npm i -D eslint \
     eslint-config-airbnb \
     eslint-config-prettier \
@@ -10,8 +17,13 @@ function initialize_and_install {
     eslint-plugin-react \
     eslint-plugin-jsx-a11y \
     eslint-plugin-prettier \
+    mocha \
+    chai \
     prettier \
-    typescript
+    ts-node \
+    typescript \
+    @types/node \
+    @types/mocha
 }
 
 function initial_commit {
@@ -19,14 +31,22 @@ function initial_commit {
   git commit -m "Initial commit"
 }
 
-function add_scripts_to_package_json {
+function config_package_json {
   mv package.json template.json
-  cat template.json | jq '.scripts += {"start":"node ."}' > package.json
+  cat template.json |
+    jq '.scripts.test = "mocha --require ts-node/register test/**/*.ts"' |
+    jq '.scripts += {"prestart":"npm run build"}' |
+    jq '.scripts += {"start":"node ."}' |
+    jq '.scripts += {"start:dev":"nodemon src/index.ts"}' |
+    jq '.scripts += {"lint":"eslint . --ext .ts,.js"}' |
+    jq '.scripts += {"pretest":"npm run lint"}' |
+    jq '.scripts += {"build":"tsc"}' |
+    jq '.main = "dist/index.js"' \
+    > package.json
   rm template.json
 }
 
-initialize_and_install
-
+function config_eslint {
 cat > .eslintrc <<- "EOF"
 {
   "extends": ["airbnb", "prettier"],
@@ -39,7 +59,9 @@ cat > .eslintrc <<- "EOF"
   }
 }
 EOF
+}
 
+function config_prettier {
 cat > .prettierrc <<- "EOF"
 {
   "arrowParens": "avoid",
@@ -58,7 +80,9 @@ cat > .prettierrc <<- "EOF"
   "useTabs": false
 }
 EOF
+}
 
+function config_typescript {
 cat > tsconfig.json <<- "EOF"
 {
   "compilerOptions": {
@@ -82,13 +106,66 @@ cat > tsconfig.json <<- "EOF"
   ]
 }
 EOF
+}
 
+function config_nodemon {
+cat > nodemon.json <<- "EOF"
+{
+  "restartable": "rs",
+  "ignore": [".git", "node_modules/**/node_modules"],
+  "verbose": true,
+  "execMap": {
+    "ts": "node --require ts-node/register"
+  },
+  "watch": ["src/"],
+  "env": {
+    "NODE_ENV": "development"
+  },
+  "ext": "js,json,ts"
+}
+EOF
+}
+
+function generate_src_code {
 mkdir src
-
 cat > src/index.ts <<- "EOF"
 console.log("Hello, World!");
 EOF
+}
 
-add_scripts_to_package_json
+function generate_test_code {
+mkdir test
+cat > test/test.ts <<- "EOF"
+const { expect } = require("chai");
 
+describe("This module", () => {
+  it("should do something", () => {
+    expect(true).to.equal(true);
+  });
+});
+EOF
+}
+
+function create_ignore_files {
+cat > .eslintignore <<- "EOF"
+dist
+EOF
+
+cat > .prettierignore <<- "EOF"
+dist
+EOF
+}
+
+mkdir $DIRECTORY && cd $DIRECTORY
+
+initialize_project
+install_dev_dependencies
+config_eslint
+config_prettier
+config_typescript
+config_package_json
+config_nodemon
+generate_src_code
+generate_test_code
+create_ignore_files
 initial_commit
---
+++ test.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+rm -rf test-project && mkdir test-project && cd test-project
+
+./../create.sh
