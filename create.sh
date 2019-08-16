2f7b99954bef332b5648b94db784b5533ee3eea5
Mark Greenwood <mark.greenwood@hatchdata.com>
2019-08-16 12:46:11 -0700

    Updated basic server code

--- create.sh
+++ create.sh
@@ -97,6 +97,11 @@ function generate_hapi_server {
   cat > src/server.ts <<- "EOF"
 import * as Hapi from "@hapi/hapi";
 
+const init = async (server: Hapi.Server) => {
+  await server.start();
+  console.log("Server running on port: ", server.info.uri);
+};
+
 const server: Hapi.Server = new Hapi.Server({ host: "localhost", port: 3000 });
 
 server.route([
@@ -107,9 +112,15 @@ server.route([
   },
 ]);
 
-server.start().then(() => {
-  console.log(`Server started and running on ${server.info.uri}`);
+process.on("unhandledRejection", err => {
+  console.log(err);
+  process.exit(1);
 });
+
+if (require.main === module) {
+  init(server);
+  console.log(`Server started and running on ${server.info.uri}`);
+}
 
 export default server;
 EOF
